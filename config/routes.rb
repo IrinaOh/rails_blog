bea879a87149af823e025116a1767bd7a67c8f0b
Irina O'Hara <irina.v.ohara@gmail.com>
2016-12-10 18:34:50 -0700

    add posts with a date

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   root to: 'users#index'
+  root to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
